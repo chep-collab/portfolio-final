cc40e35092fc375b67046e5815da86453c8290fa
Mercy chepngeno <mercychepngeno582@gmail.com>
2025-07-17 14:49:53 +0300

    Add Tailwind Vite plugin and import global CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwind()],
 })
