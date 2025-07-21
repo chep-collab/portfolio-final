aefa21ee51ec4fed8027c3a11b3bc5900d17fc5d
Mercy chepngeno <mercychepngeno582@gmail.com>
2025-07-21 23:17:37 +0300

    Fix Tailwind styling by switching to Tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {},
   },
