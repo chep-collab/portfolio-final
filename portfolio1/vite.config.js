537c02432c6a0005f998e85a0c3bbee3d703b1c2
Mercy chepngeno <mercychepngeno582@gmail.com>
2025-07-22 10:28:07 +0300

    Remove portfolio1 project from repo

--- portfolio1/vite.config.js
+++ portfolio1/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tailwind()
-  ]
+  plugins: [vue()],
 })
